5ed2088cd7e6ac6e992862d000816c5f0035946b
Zekerijah <zekerijahsehic1@gmail.com>
2021-11-17 17:37:57 +0100

    Fix method add @Transact...

--- src/main/java/com/zekerijah/eventdemo/service/TicketService.java
+++ src/main/java/com/zekerijah/eventdemo/service/TicketService.java
@@ -4,6 +4,7 @@ import com.zekerijah.eventdemo.domain.Ticket;
 import com.zekerijah.eventdemo.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,22 +15,27 @@ public class TicketService {
     @Autowired
     private TicketRepository ticketRepository;
 
+    @Transactional
     public List<Ticket> findAllTicket(){
         return ticketRepository.findAll();
     }
 
+    @Transactional
     public Optional<Ticket> findTicket(Integer id){
         return ticketRepository.findById(id);
     }
 
-    public void saveTicket (Ticket ticket){
-        ticketRepository.save(ticket);
+    @Transactional
+    public Ticket saveTicket (Ticket ticket){
+        return ticketRepository.save(ticket);
     }
 
+    @Transactional
     public void updateTicket (Ticket ticket) {
         ticketRepository.save(ticket);
     }
 
+    @Transactional
     public void deleteTicket (Integer id) {
         ticketRepository.deleteById(id);
     }
