d4abe86ceb80fd451d22b6eda81efbd6fb1fe975
Zekerijah <zekerijahsehic1@gmail.com>
2021-11-19 13:48:57 +0100

    Add new period

--- src/test/java/com/zekerijah/eventdemo/PeriodUtil.java
+++ src/test/java/com/zekerijah/eventdemo/PeriodUtil.java
@@ -17,16 +17,6 @@ public class PeriodUtil {
                 .build();
     }
 
-    public static Period generatePeriodWithStartDateBeforeNow() {
-
-        return Period.builder()
-                .startDate(LocalDate.now().minusDays(1))
-                .endDate(LocalDate.now().plusDays(2))
-                .startTime(Time.valueOf(LocalTime.now().plusHours(1)))
-                .endTime(Time.valueOf(LocalTime.now().plusHours(2)))
-                .build();
-    }
-
     public static Period generatePeriodWithEndDateBeforeStartDate() {
         return Period.builder()
                 .startDate(LocalDate.now())
@@ -36,19 +26,10 @@ public class PeriodUtil {
                 .build();
     }
 
-    public static Period generatePeriodWithStartTimeBeforeNow() {
+    public static Period generatePeriodPresentDateEndTimeBeforeStartTime() {
         return Period.builder()
                 .startDate(LocalDate.now())
-                .endDate(LocalDate.now().plusDays(1))
-                .startTime(Time.valueOf(LocalTime.now()))
-                .endTime(Time.valueOf(LocalTime.now().plusHours(2)))
-                .build();
-    }
-
-    public static Period generatePeriodWithEndTimeBeforeStartTime() {
-        return Period.builder()
-                .startDate(LocalDate.now())
-                .endDate(LocalDate.now().plusDays(1))
+                .endDate(LocalDate.now())
                 .startTime(Time.valueOf(LocalTime.now().plusHours(2)))
                 .endTime(Time.valueOf(LocalTime.now().plusHours(1)))
                 .build();
