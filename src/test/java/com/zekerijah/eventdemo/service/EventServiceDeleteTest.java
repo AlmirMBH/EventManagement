726e6a624404a18a0f01c45d505ff9b9fe1e5f6c
Zekerijah <zekerijahsehic1@gmail.com>
2021-11-23 10:59:53 +0100

    Test Delete Event

---
+++ src/test/java/com/zekerijah/eventdemo/service/EventServiceDeleteTest.java
@@ -0,0 +1,12 @@
+package com.zekerijah.eventdemo.service;
+
+import com.zekerijah.eventdemo.IntegrationTest;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EventServiceDeleteTest extends IntegrationTest {
+
+    @Autowired
+    private EventService eventService;
+
+
+}
