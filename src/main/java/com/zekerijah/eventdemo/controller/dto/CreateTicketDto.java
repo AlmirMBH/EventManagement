c97391fd8f1da746dc396df20a032b25561f6d4b
Zekerijah <zekerijahsehic1@gmail.com>
2021-11-23 10:56:52 +0100

    CreateTicketDto

---
+++ src/main/java/com/zekerijah/eventdemo/controller/dto/CreateTicketDto.java
@@ -0,0 +1,36 @@
+package com.zekerijah.eventdemo.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CreateTicketDto {
+    @NotBlank
+    private String name;
+    @NotNull
+    private Double price;
+    @NotNull
+    private Integer quantityAvailable;
+    @NotNull
+    private PeriodDto period;
+
+    @Override
+    public String toString() {
+        return "CreateTicketDto{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                ", quantityAvailable=" + quantityAvailable +
+                ", period=" + period +
+                '}';
+    }
+}
